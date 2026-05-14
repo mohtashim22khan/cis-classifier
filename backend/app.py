e98640b42451729cc9b8aac311efd82f4b14d0b2
mohtashim22khan <muhd.mohtashim@gmail.com>
2026-05-15 02:33:21 +0500

    hopefully

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cis-classifier.vercel.app/",
-                   "http://localhost:5173"],
+    allow_origins=["https://cis-classifier.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
